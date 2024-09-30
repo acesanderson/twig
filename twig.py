77eb3efa2e21d8fc701a473567da0f0249f535ba
Brian Anderson <brian.briananderson@gmail.com>
2024-09-29 22:05:19 -0500

    fixed bug where context was showing up as none instead of '' when nonpiped

--- twig.py
+++ twig.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         # We add this as context to the query
         context = f"\n<context>\n{context}</context>"
     else:
-        context = None
+        context = ""
     # Capture arguments
     parser = argparse.ArgumentParser(description="Twig: A lightweight command line interface for Chain framework.")
     parser.add_argument("query", type=str, nargs="?", help="Query string to be executed.")
